35cfceb38b69124464dfee1b475c829b58d4e14f
zigfrei <zigfrei.zf@gmail.com>
2016-09-18 02:59:39 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 17.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Epam training project");
+        System.out.println("tipok razonik");
+    }
+}
